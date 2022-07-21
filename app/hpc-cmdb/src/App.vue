7e6f285beefca1d8414b555683d8127eca620806
Will-max-bit <whermann4@gmail.com>
2022-07-20 20:40:54 -0700

    Still having cors issues

---
+++ app/hpc-cmdb/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{ quote }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { ref } from 'vue'
+export default{
+  setup(){
+    
+    const quote = ref('')
+    axios.get('http://localhost:8000/entries/')
+    .then(response => {
+      quote.value = response
+    })
+    return {
+      quote
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/scripts.js
@@ -0,0 +1,32 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        entries: [],
+
+        new_entry: "",
+        new_tags: [],
+        tag_name: "",
+        tag_value: "",
+    },
+    methods: {
+        query: function () {
+            axios({
+                url: 'http://localhost:8000/entries/',
+                method: 'get',
+            }).then(response => {
+                console.log('burrs')
+                this.entries = response.data;
+            })
+        },
+        addEntry: function () {
+            axios({
+                url: 'http://localhost:8000/entries/',
+                method: 'post',
+                data: {"name": this.new_entry, "tags": [{"name": this.tag_name, "value": this.tag_value}]}
+            })
+        }
+    },
+    created: function () {
+        this.query()
+    }
+})
