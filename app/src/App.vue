e4ca9b854b2abfe7e3f9cfa0d6c913a44cdcba73
Lucas Crownover <lcrownover127@gmail.com>
2022-07-21 20:33:05 -0700

    eslint

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,37 +1,37 @@
 <template>
-<h1> HEY THERE GUY </h1><br>
-<h2> HOWDY FRIEND </h2><br>
-    <button @click="addBoy">Submit</button><br>
-        <h3>{{boy}}</h3>
-    <input type="text" v-model="name">
-  {{quote}}
+  <h1> HEY THERE GUY </h1><br>
+  <h2> HOWDY FRIEND </h2><br>
+  <button @click="addBoy">Submit</button><br>
+  <h3>{{ boy }}</h3>
+  <input type="text" v-model="name">
+  {{ quote }}
 </template>
 
 <script>
 import axios from 'axios'
 import { ref } from 'vue'
-export default{
+export default {
   methods: {
-      addBoy(){
-        const boy = ref('')
-        axios.post('http://localhost:8000/entries/', {
-            name: "batman",
-            tags: [{name: "love-interest", value: "alfred"}]
-        })
+    addBoy() {
+      const boy = ref('')
+      axios.post('http://localhost:8000/entries/', {
+        name: "batman",
+        tags: [{ name: "love-interest", value: "alfred" }]
+      })
         .then(response => {
           boy.value = response
         })
-        return {
-          boy
-        }
+      return {
+        boy
       }
+    }
   },
-  setup(){
+  setup() {
     const quote = ref('')
     axios.get('http://localhost:8000/entries/')
-    .then(response => {
-      quote.value = response
-    })
+      .then(response => {
+        quote.value = response
+      })
     return {
       quote
     }
